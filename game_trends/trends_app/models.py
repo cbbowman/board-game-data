b5819c8c8e4c5de38ed16be613d9e44ee880c6bb
Charlie Bowman <c.b.bowman@outlook.com>
2021-09-16 11:29:35 -0400

    debug

--- game_trends/trends_app/models.py
+++ game_trends/trends_app/models.py
@@ -84,7 +84,8 @@ def checkTopGames():
 	low_ranked_games = Game.objects.all().annotate(total_rank=F('play_rank')+F('growth_rank')+F('h_rank')+F('h_growth_rank')).order_by('-total_rank')
 	if len(low_ranked_games)>max_size:
 		for i in range(len(low_ranked_games)-max_size):
-			pass
+			low_ranked_games[i].delete()
+			# pass
 			# if low_ranked_games[i].fav_users.all().count()>0:
 			# 	continue
 			# else:
