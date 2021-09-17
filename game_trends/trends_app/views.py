2021ae444c7c870ea8606058120df80a3fea9e81
Charlie Bowman <c.b.bowman@outlook.com>
2021-09-17 15:28:34 -0400

    debug

--- game_trends/trends_app/views.py
+++ game_trends/trends_app/views.py
@@ -5,7 +5,19 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.sessions.models import Session
 from django.db.models import F
-from .tasks import scrape_games
+# from .tasks import scrape_games
+from background_task import background
+from background_task.models import Task
+from .models import addNewGame, checkTopGames
+
+@background(schedule=5)
+def scrape_games():
+    tasks = Task.objects.filter(verbose_name="scraper")
+    if len(tasks) == 0:
+        checkTopGames()
+        pass
+    else:
+        pass
 
 def delete_all(request):
 	Game.objects.all().delete()
