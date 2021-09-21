8071951a835e876be3767ba02b90efb8b138d01e
Charlie Bowman <c.b.bowman@outlook.com>
2021-09-21 09:56:06 -0400

    add overall rank fields

--- game_trends/trends_app/tasks.py
+++ game_trends/trends_app/tasks.py
@@ -2,14 +2,14 @@ from background_task import background
 from background_task.models import Task
 from .models import addNewGame, checkTopGames
 
-# @background(schedule=5)
-# def scrape_games():
-#     tasks = Task.objects.filter(verbose_name="scraper")
-#     if len(tasks) == 0:
-#         checkTopGames()
-#         pass
-#     else:
-#         pass
+@background(schedule=5)
+def scrape_games():
+    tasks = Task.objects.filter(verbose_name="scraper")
+    if len(tasks) == 0:
+        checkTopGames()
+        pass
+    else:
+        pass
 
 # @background(schedule=5)
 # def add_this_game():
